e9566972028f1de2ccb72e31114f7e08d46708ab
Eunju/kiori <eun01300714@gmail.com>
2023-08-28 21:48:23 +0900

    #5 3. Work experience content

--- src/components/ProjectComponent/ProjectComponent.vue
+++ src/components/ProjectComponent/ProjectComponent.vue
@@ -8,37 +8,45 @@
     <div class="content">
       <h2 class="name">{{ article.name }}</h2>
       <div v-for="item in article.description" class="description" v-html="item"></div>
-      <div v-if="article.projects">
-        <div v-for="project in article.projects" class="project">
-          <div class="title">
-            <div class="topic">{{ project.topic }}</div>
-            <div class="name" v-html="project.name"></div>
-          </div>
-          <div class="period">
-            <div class="period-start">{{ project.periodStart }}</div>
-            <div class="period-end">{{ project.periodEnd }}</div>
-          </div>
-          <div class="position">
-            <p>{{ positionText }}</p>{{ project.position }}
-          </div>
-          <div v-for="description in project.description" class="description">{{ description }}</div>
-          <ul v-if="project.detail" class="detail">
-            <li v-for="item in project.detail" v-html="item"></li>
-          </ul>
-          <div v-if="project.techStack" class="techStack">
-            <div>Tech Stack</div>
-            <ul>
-              <li v-for="stack in project.techStack">
-                <span class="name">{{ stack.name }}</span>
-                <span v-for="item in stack.list" class="item">{{ item }}</span>
-              </li>
-            </ul>
-          </div>
-          <div v-if="project.images" class="images">
-            <img>
-          </div>
-        </div>
-      </div>
+      <table class="project">
+        <template v-for="project in article.projects">
+          <tr>
+            <td class="period-start">{{ project.periodStart }}</td>
+            <td class="title">
+              <div class="topic">{{ project.topic }}</div>
+              <div class="name" v-html="project.name"></div>
+            </td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>
+              <div class="period">
+                <div class="period-start">{{ project.periodStart }}</div>
+                <div class="period-end">{{ project.periodEnd }}</div>
+              </div>
+              <div class="position">
+                <p>{{ positionText }}</p>{{ project.position }}
+              </div>
+              <div v-for="description in project.description" class="description">{{ description }}</div>
+              <ul v-if="project.detail" class="detail">
+                <li v-for="item in project.detail" v-html="item"></li>
+              </ul>
+              <div v-if="project.techStack" class="techStack">
+                <div>Tech Stack</div>
+                <ul>
+                  <li v-for="stack in project.techStack">
+                    <span class="name">{{ stack.name }}</span>
+                    <span v-for="item in stack.list" class="item">{{ item }}</span>
+                  </li>
+                </ul>
+              </div>
+              <div v-if="project.images" class="images">
+                <img>
+              </div>
+            </td>
+          </tr>
+        </template>
+      </table>
     </div>
   </div>
 </template>
